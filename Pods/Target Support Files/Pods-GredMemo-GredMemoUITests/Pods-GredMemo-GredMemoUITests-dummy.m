93f1b95c6aa377d95c73a2a77262d5606cc57437
Gred <jihong.park.dev@gmail.com>
2020-02-25 20:32:44 +0900

    Pod delete

--- Pods/Target Support Files/Pods-GredMemo-GredMemoUITests/Pods-GredMemo-GredMemoUITests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_GredMemo_GredMemoUITests : NSObject
-@end
-@implementation PodsDummy_Pods_GredMemo_GredMemoUITests
-@end
--- Pods/Target Support Files/Pods-GredMemo/Pods-GredMemo-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_GredMemo : NSObject
-@end
-@implementation PodsDummy_Pods_GredMemo
-@end
